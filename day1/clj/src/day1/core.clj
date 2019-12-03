1119c0bd2f912a100bed015c1b7e40da625353ef
ABuffSeagull <reecevanatta@gmail.com>
2019-12-02 23:37:31 -0500

    Some formatting and refactoring

--- day1/clj/src/day1/core.clj
+++ day1/clj/src/day1/core.clj
@@ -2,19 +2,23 @@
   (:require [clojure.string :as string])
   (:gen-class))
 
-(defn part-1
-  [numbers]
-  (->>
-   numbers
-   (map #(- (Math/floor (/ % 3)) 2))
-   (reduce +)))
+(defn fuel-from-mass
+  "Calculates the fuel needed for given mass"
+  [mass]
+  (- (Math/floor (/ mass 3)) 2))
+
+(defn part-1 [numbers]
+  (->> numbers
+       (map fuel-from-mass)
+       (reduce +)))
+
+(defn part-2 [numbers])
 
 (defn -main
   [file]
-  (->>
-   file
-   (slurp)
-   (string/split-lines)
-   (map #(Integer/parseInt %))
-   (part-1)
-   (println)))
+  (->> file
+       (slurp)
+       (string/split-lines)
+       (map #(Integer/parseInt %))
+       (part-1)
+       (println)))
