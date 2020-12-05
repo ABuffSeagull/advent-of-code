b51eef7233ab5460878798b6e1a5cbcb792df419
Reece Van Atta <reecevanatta@hey.com>
2020-12-05 16:20:50 -0500

    Day 5 Part 2

--- 2020/day5/ex/day5.ex
+++ 2020/day5/ex/day5.ex
@@ -5,6 +5,20 @@ defmodule Day5 do
     |> Enum.max()
   end
 
+  def part2(input) do
+    occupied_seats =
+      Stream.map(input, &get_seat_id/1)
+      |> Enum.sort()
+
+    # Find the first occupied seat where the next seat is unoccupied,
+    # as that will be our seat.
+    {empty_seat, _} =
+      Stream.zip(occupied_seats, Stream.drop(occupied_seats, 1))
+      |> Enum.find(fn {seat, next_seat} -> seat + 1 != next_seat end)
+
+    empty_seat + 1
+  end
+
   def get_seat_id(directions) do
     # Split into row and column directions
     {rows, columns} =
@@ -12,6 +26,13 @@ defmodule Day5 do
       |> String.graphemes()
       |> Enum.split(7)
 
+    # Basically we're building a number in binary
+    # First we make a descending list of powers of 2, ending with 1.
+    # Then we zip it with the letters of the directions
+    # We then filter it down to only the "back" directions, as they add to the
+    # final row number
+    # Then sum them up to the get the final number.
+    # Simple!
     row_num =
       Range.new(length(rows) - 1, 0)
       |> Stream.map(&:math.pow(2, &1))
