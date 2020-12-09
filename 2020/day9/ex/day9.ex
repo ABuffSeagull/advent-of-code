e2c6ff3225e1e845cf8159fc4d3f2c155d000dbf
ABuffSeagull <reecevanatta@gmail.com>
2020-12-09 13:12:01 -0500

    2020 Day 9 Part 2

--- 2020/day9/ex/day9.ex
+++ 2020/day9/ex/day9.ex
@@ -6,8 +6,7 @@ defmodule Day9 do
       |> Enum.map(&String.to_integer/1)
 
     bad_index =
-      0..(length(numbers) - 1)
-      |> Stream.drop(preamble_length)
+      preamble_length..(length(numbers) - 1)
       |> Enum.find(&valid_number?(numbers, preamble_length, &1))
 
     Enum.at(numbers, bad_index)
@@ -16,12 +15,40 @@ defmodule Day9 do
   def valid_number?(numbers, preamble_length, index) do
     current_num = Enum.at(numbers, index)
 
-    current_slice =
-      numbers
-      |> Enum.slice(index - preamble_length, preamble_length)
+    current_slice = Enum.slice(numbers, index - preamble_length, preamble_length)
 
     not Enum.any?(current_slice, fn number ->
       (current_num - number) in current_slice
+    end)
+  end
+
+  def part2(input, preamble_length) do
+    bad_number = part1(input, preamble_length)
+
+    {min, max} =
+      input
+      |> String.splitter("\n", trim: true)
+      |> Enum.map(&String.to_integer/1)
+      |> Stream.iterate(&tl/1)
+      |> Stream.map(&subtract_to_non_positive(&1, bad_number))
+      |> Enum.find_value(fn
+        {0, nums} -> nums
+        _ -> nil
+      end)
+      |> Enum.min_max()
+
+    min + max
+  end
+
+  def subtract_to_non_positive(num_list, bad_number) do
+    Enum.reduce_while(num_list, {bad_number, []}, fn num, {acc, nums} ->
+      new_acc = acc - num
+
+      if new_acc <= 0 do
+        {:halt, {new_acc, nums}}
+      else
+        {:cont, {new_acc, [num | nums]}}
+      end
     end)
   end
 end
