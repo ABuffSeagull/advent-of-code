1378741cc4355b5b653662d9e493647979d2f7a8
ABuffSeagull <reecevanatta@hey.com>
2020-12-12 18:19:54 -0500

    2020 Day 10 Part 2 FINALLY

--- 2020/day10/ex/day10.ex
+++ 2020/day10/ex/day10.ex
@@ -14,4 +14,26 @@ defmodule Day10 do
 
     distribution[1] * distribution[3]
   end
+
+  def part2(input) do
+    nums =
+      input
+      |> String.splitter("\n", trim: true)
+      |> Enum.map(&String.to_integer/1)
+
+    adapters = Enum.sort([0 | nums], :desc)
+
+    Stream.zip([List.first(adapters) + 3 | adapters], adapters)
+    |> Stream.map(fn {first, second} -> first - second end)
+    |> Stream.chunk_by(& &1)
+    |> Stream.reject(&(3 in &1))
+    |> Stream.map(&Enum.count/1)
+    |> Stream.map(fn
+      4 -> 7
+      3 -> 4
+      2 -> 2
+      1 -> 1
+    end)
+    |> Enum.reduce(&Kernel.*/2)
+  end
 end
