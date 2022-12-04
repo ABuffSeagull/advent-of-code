27989cc4f1529021eb5bb0719a988a70bad1dd03
ABuffSeagull <reecevanatta@hey.com>
2022-12-03 19:00:09 -0500

    2022 Day 3 Part 1

---
+++ 2022/day03/zig/src/main.zig
@@ -0,0 +1,39 @@
+const std = @import("std");
+
+// heh, good pun
+const AlphaBitSet = std.StaticBitSet(26 * 2);
+
+pub fn main() !void {
+    var input_buffer = [_]u8{undefined} ** (1024 * 1024);
+
+    const input = try std.fs.cwd().readFile("../input.txt", &input_buffer);
+
+    var line_it = std.mem.tokenize(u8, input, "\n");
+
+    var priority_sum: usize = 0;
+    while (line_it.next()) |line| {
+        var first_set = AlphaBitSet.initEmpty();
+        // first compartment
+        for (line[0 .. line.len / 2]) |char| {
+            if (char < 'a') {
+                first_set.set(char - 'A' + 26);
+            } else {
+                first_set.set(char - 'a');
+            }
+        }
+        var second_set = AlphaBitSet.initEmpty();
+        // second compartment
+        for (line[line.len / 2 ..]) |char| {
+            if (char < 'a') {
+                second_set.set(char - 'A' + 26);
+            } else {
+                second_set.set(char - 'a');
+            }
+        }
+
+        first_set.setIntersection(second_set);
+        priority_sum += first_set.findFirstSet().? + 1;
+    }
+
+    std.debug.print("sum: {}\n", .{priority_sum});
+}
