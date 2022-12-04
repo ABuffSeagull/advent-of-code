c81886cd3e953d7d566ca91512d635e457bccb62
ABuffSeagull <reecevanatta@hey.com>
2022-12-03 19:10:29 -0500

    2022 Day 3 Part 2

--- 2022/day03/zig/src/main.zig
+++ 2022/day03/zig/src/main.zig
@@ -8,6 +8,11 @@ pub fn main() !void {
 
     const input = try std.fs.cwd().readFile("../input.txt", &input_buffer);
 
+    part1(input);
+    part2(input);
+}
+
+fn part1(input: []const u8) void {
     var line_it = std.mem.tokenize(u8, input, "\n");
 
     var priority_sum: usize = 0;
@@ -35,5 +40,46 @@ pub fn main() !void {
         priority_sum += first_set.findFirstSet().? + 1;
     }
 
-    std.debug.print("sum: {}\n", .{priority_sum});
+    std.debug.print("part1: {}\n", .{priority_sum});
+}
+
+fn part2(input: []const u8) void {
+    var line_it = std.mem.tokenize(u8, input, "\n");
+
+    var priority_sum: usize = 0;
+    while (line_it.next()) |first_line| {
+        var first_set = AlphaBitSet.initEmpty();
+        for (first_line) |char| {
+            if (char < 'a') {
+                first_set.set(char - 'A' + 26);
+            } else {
+                first_set.set(char - 'a');
+            }
+        }
+
+        var second_set = AlphaBitSet.initEmpty();
+        for (line_it.next().?) |char| {
+            if (char < 'a') {
+                second_set.set(char - 'A' + 26);
+            } else {
+                second_set.set(char - 'a');
+            }
+        }
+
+        var third_set = AlphaBitSet.initEmpty();
+        for (line_it.next().?) |char| {
+            if (char < 'a') {
+                third_set.set(char - 'A' + 26);
+            } else {
+                third_set.set(char - 'a');
+            }
+        }
+
+        first_set.setIntersection(second_set);
+        first_set.setIntersection(third_set);
+
+        priority_sum += first_set.findFirstSet().? + 1;
+    }
+
+    std.debug.print("part2: {}\n", .{priority_sum});
 }
