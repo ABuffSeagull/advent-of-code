96f02031a7374dbd4c332105764524bcb4414bcf
ABuffSeagull <reecevanatta@hey.com>
2022-12-06 22:00:18 -0500

    2022 Day 6 part 2

--- 2022/day06/zig/src/main.zig
+++ 2022/day06/zig/src/main.zig
@@ -4,18 +4,25 @@ pub fn main() !void {
     var input_buffer = [_]u8{undefined} ** (1024 * 1024);
     const input = try std.fs.cwd().readFile("../input.txt", &input_buffer);
 
+    std.debug.print("part1: {}, part2: {}\n", .{
+        findMarker(input, 4),
+        findMarker(input, 14),
+    });
+}
+
+fn findMarker(input: []const u8, unique_char_count: usize) usize {
     var index: usize = 0;
-    while (index + 4 < input.len) : (index += 1) {
-        const slice = input[index .. index + 4];
+    while (index + unique_char_count < input.len) : (index += 1) {
+        const slice = input[index .. index + unique_char_count];
 
         var bit_set = std.StaticBitSet(std.math.maxInt(u8)).initEmpty();
 
         for (slice) |char| bit_set.set(char);
 
-        if (bit_set.count() == 4) {
+        if (bit_set.count() == unique_char_count) {
             break;
         }
     }
 
-    std.debug.print("first marker: {}\n", .{index + 4});
+    return index + unique_char_count;
 }
